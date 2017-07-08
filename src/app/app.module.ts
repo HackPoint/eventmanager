fb5e7532b183487e90080ef6315daadb8c84e11c
hackp0int <genashm@gmail.com>
2017-07-08 22:38:02 +0300

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LOG_LOGGER_PROVIDERS } from 'angular2-logger/core';
-import { AppComponent } from './app.component';
-import { ListenerComponent } from './listener/listener.component';
-import { SenderComponent } from './sender/sender.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import { Logger} from 'angular2-logger/core';
+import {AppComponent} from './app.component';
+import {ListenerComponent} from './listener/listener.component';
+import {SenderComponent} from './sender/sender.component';
 import {MessageHubService} from './shared/messagebroker/messagehub.service';
 
 @NgModule({
@@ -15,7 +15,8 @@ import {MessageHubService} from './shared/messagebroker/messagehub.service';
   imports: [
     BrowserModule,
   ],
-  providers: [LOG_LOGGER_PROVIDERS, MessageHubService],
+  providers: [ MessageHubService, Logger],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
